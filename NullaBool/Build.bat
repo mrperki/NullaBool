24dea08552a6ec6c1e22a1bb47abefd054a33330
Matt Perkins <matt.perkins@phsa.ca>
2018-05-15 10:56:44 -0700

    Added a build script and added the file output to the nuspec, let's get NullaBool into the hands of the people who don't need it!

---
+++ NullaBool/Build.bat
@@ -0,0 +1,24 @@
+@echo Off
+REM This is a build script for MyGet to build and package a NuGet
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild NullaBool.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net461
+
+%nuget% pack "NullaBool.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
